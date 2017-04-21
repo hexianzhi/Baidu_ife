fe9b204ceeb5775ba7b6ea1a502d3878e3537365
hexianzhi <hxz707950194@gmail.com>
2017-04-22 02:53:51 +0800

    完成任务21_2

--- task21/task20.js
+++ task21/task20.js
@@ -15,52 +15,64 @@ function addEvent(element, event, listener) {
 }
 
 var tagInput = document.getElementById("tag-input");
-
-var tagOb = {
-
-    tagContent: document.getElementById("tag-content"),
+var tagContent = document.getElementById("tag-content");
+var tagObj = {
     tags: [],
-
 
     MonitorTagInput: function () {
         //监听输入框的状态
-        var s = tagOb.tagInput.value;
-        if (s.match(/\w/)) {
-            var s2 = s;
-            if (s.match(/\W$/)) {
-                //添加tag
-                tagOb.tags.push(s2);
-                tagOb.renderTags();
+        if (/[,，;；、\s\n]+/.test(tagInput.value) || event.keyCode == 13) {
+            var data = tagInput.value.trim().split(/[,，;；、。.\s\n]+/);
+            var newTag = data[0];
+
+            //去重
+            if (tagObj.tags.indexOf(newTag) === -1) {
+                tagObj.tags.push(newTag);
+                if (tagObj.tags.length > 10) {
+                    tagObj.tags.shift();
+                }
             }
-        } else {
-            tagOb.tagInput.value = "";
+            tagObj.renderTags();
+            tagInput.value = "";
         }
+
+
+        // if (/\w/.test(tagInput.value)) {
+        //     var s2 = s;
+        //     if (s.test(/\W$/)) {
+        //         //添加tag
+        //         tagObj.tags.push(s2);
+        //         tagObj.renderTags();
+        //     }
+        // } else {
+        //     tagInput.value = "";
+        // }
     },
 
 
     deleteTag: function (element) {
-        tagOb.tags.splice(tags.indexOf(element.value), 1);
+        tagObj.tags.splice(tags.indexOf(element.value), 1);
     },
 
     renderTags: function () {
         var str = "";
-        tagOb.tagContent.innerHTML = "";
-        tagOb.tags.forEach(function (p1) {
+        tagContent.innerHTML = "";
+        tagObj.tags.forEach(function (p1) {
             str += "<span class='tag'>" + p1 + "</span>"
         });
-        tagOb.tagContent.innerHTML = str;
+        tagContent.innerHTML = str;
     }
 
 }
 
 
 function bindClick() {
-    var tag = document.getElementById("tag");
+    var tag = document.getElementById("tag-content");
 
     //添加 hover 事件，只能通过 div 父控件来。。
     addEvent(tag, 'mouseover', function (e) {
         if (e.target && e.target.nodeName == "SPAN") {
-            e.target.innerHTML.insertData(0, '删除');
+            e.target.firstChild.insertData(0, '删除');
             e.target.style.background = 'red';
         }
     });
@@ -68,19 +80,20 @@ function bindClick() {
     //添加删除事件
     addEvent(tag, "click", function (e) {
         if (e.target && e.target.nodeName === "SPAN") {
-            tagOb.deleteTag(e.target);
+            tagObj.deleteTag(e.target);
         }
     });
 
     //添加监听 input 事件
-    addEvent(tagInput, 'keyup', tagOb.MonitorTagInput());
+    // 当一个按键被释放时，keyup事件被触发。
+    addEvent(tagInput, 'keyup', tagObj.MonitorTagInput);
 
 
     var interstContent = document.getElementById("interst-content");
     var comfirmInterst = document.getElementById("comfirm-interst");
     var interstContent = document.getElementById("interest-content");
 
-    var arrBtnDel = table.getElementsByClassName("del-btn");
+    var arrBtnDel = document.getElementsByClassName("del-btn");
 
 
 }
