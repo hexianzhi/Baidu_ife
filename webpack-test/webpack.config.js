d114d9cafe554811aa2fcf12a0b87f4bab803f35
hexianzhi <hxz707950194@gmail.com>
2017-08-21 23:48:04 +0800

    增加热更新模块，原模块有缺陷

--- webpack-test/webpack.config.js
+++ webpack-test/webpack.config.js
@@ -2,11 +2,18 @@
  * Created by gedune on 2017/8/17.
  */
 module.exports = {
-  entry: './app/main.js',
+  entry: './app/main.js','webpack-hot-middleware/client',
   output: {
     path: "/public",//打包后的文件存放的地方
     filename: 'bundle.js'
   },
+  plugins: [
+    // OccurenceOrderPlugin is needed for webpack 1.x only
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+
 
   devServer: {
     contentBase: "./public",//本地服务器所加载的页面所在的目录
